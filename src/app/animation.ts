178c43382acb72ceaf6ad3c2e5bcaa483711e02d
thanhtutzaw <zgyi5025@gmail.com>
2024-02-09 20:17:05 +0630

    fix : scroll bar

--- src/app/animation.ts
+++ src/app/animation.ts
@@ -9,49 +9,49 @@ import {
 } from '@angular/animations';
 
 export const slideInAnimation = trigger('routeAnimations', [
-  transition('HomePage <=> WelcomePage', [
-    style({ position: 'relative' }),
-    query(':enter, :leave', [
-      style({
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%',
-      }),
-    ]),
-    query(':enter', [style({ left: '-100%' })], { optional: true }),
-    query(':leave', animateChild(), { optional: true }),
-    group([
-      query(':leave', [animate('300ms ease-out', style({ left: '100%' }))], {
-        optional: true,
-      }),
-      query(':enter', [animate('300ms ease-out', style({ left: '0%' }))], {
-        optional: true,
-      }),
-    ]),
-  ]),
-  transition('WelcomePage <=> LoginPage', [
-    style({ position: 'relative' }),
-    query(':enter, :leave', [
-      style({
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%',
-        opacity: 1,
-      }),
-    ]),
-    query(':enter', [style({ opacity: 0 })], { optional: true }),
-    query(':leave', animateChild(), { optional: true }),
-    group([
-      query(':leave', [animate('300ms ease-out', style({ opacity: 0 }))], {
-        optional: true,
-      }),
-      query(':enter', [animate('300ms ease-out', style({ opacity: 1 }))], {
-        optional: true,
-      }),
-    ]),
-  ]),
+  // transition('HomePage <=> WelcomePage', [
+  //   style({ position: 'relative' }),
+  //   query(':enter, :leave', [
+  //     style({
+  //       position: 'absolute',
+  //       top: 0,
+  //       left: 0,
+  //       width: '100%',
+  //     }),
+  //   ]),
+  //   query(':enter', [style({ left: '-100%' })], { optional: true }),
+  //   query(':leave', animateChild(), { optional: true }),
+  //   group([
+  //     query(':leave', [animate('300ms ease-out', style({ left: '100%' }))], {
+  //       optional: true,
+  //     }),
+  //     query(':enter', [animate('300ms ease-out', style({ left: '0%' }))], {
+  //       optional: true,
+  //     }),
+  //   ]),
+  // ]),
+  // transition('WelcomePage <=> LoginPage', [
+  //   style({ position: 'relative' }),
+  //   query(':enter, :leave', [
+  //     style({
+  //       position: 'absolute',
+  //       top: 0,
+  //       left: 0,
+  //       width: '100%',
+  //       opacity: 1,
+  //     }),
+  //   ]),
+  //   query(':enter', [style({ opacity: 0 })], { optional: true }),
+  //   query(':leave', animateChild(), { optional: true }),
+  //   group([
+  //     query(':leave', [animate('300ms ease-out', style({ opacity: 0 }))], {
+  //       optional: true,
+  //     }),
+  //     query(':enter', [animate('300ms ease-out', style({ opacity: 1 }))], {
+  //       optional: true,
+  //     }),
+  //   ]),
+  // ]),
   transition('* <=> *', [
     style({ position: 'relative' }),
     query(
@@ -62,22 +62,45 @@ export const slideInAnimation = trigger('routeAnimations', [
           top: 0,
           left: 0,
           width: '100%',
+          opacity: 1,
         }),
       ],
       { optional: true }
     ),
-    query(':enter', [style({ left: '-100%' })], { optional: true }),
+    // query(
+    //   ':enter, :leave',
+    //   [
+    //     style({
+    //       position: 'absolute',
+    //       top: 0,
+    //       left: 0,
+    //       width: '100%',
+    //     }),
+    //   ],
+    //   { optional: true }
+    // ),
+    // query(':enter', [style({ left: '-100%' })], { optional: true }),
+    query(':enter', [style({ opacity: 0 })], { optional: true }),
     query(':leave', animateChild(), { optional: true }),
     group([
-      query(
-        ':leave',
-        [animate('200ms ease-out', style({ left: '100%', opacity: 0 }))],
-        { optional: true }
-      ),
-      query(':enter', [animate('300ms ease-out', style({ left: '0%' }))], {
+      query(':leave', [animate('300ms ease-out', style({ opacity: 0 }))], {
+        optional: true,
+      }),
+      query(':enter', [animate('300ms ease-out', style({ opacity: 1 }))], {
         optional: true,
       }),
       query('@*', animateChild(), { optional: true }),
     ]),
+    // group([
+    //   query(
+    //     ':leave',
+    //     [animate('200ms ease-out', style({ left: '100%', opacity: 0 }))],
+    //     { optional: true }
+    //   ),
+    //   query(':enter', [animate('300ms ease-out', style({ left: '0%' }))], {
+    //     optional: true,
+    //   }),
+    //   query('@*', animateChild(), { optional: true }),
+    // ]),
   ]),
 ]);
